2e11d55ceec76e05a1b43914b08a12f669f3ead1
Nikita Bolshakov <bnikmik@gmail.com>
2023-01-23 20:04:46 +0300

    Курсовая работа 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,45 @@
+import enums.Type;
+import exceptions.TaskNotFoundExceptions;
+import services.TaskService;
+import task.DailyTask;
+import task.MonthlyTask;
+import task.OneTimeTask;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
+        DailyTask dailyTask = new DailyTask(
+                "dailyTask",
+                Type.PERSONAL,
+                "сделай это",
+                LocalDateTime.of(2023,1,25,23,10)
+        );
+        OneTimeTask oneTimeTask = new OneTimeTask(
+                "oneTimeTask",
+                Type.PERSONAL,
+                "сделай это",
+                LocalDateTime.of(2023,12,13,13,5)
+        );
+        MonthlyTask monthlyTask = new MonthlyTask(
+                "dsad",
+                Type.PERSONAL,
+                "сделай это",
+                LocalDateTime.of(2023,4,13,13,5)
+        );
+
+        TaskService taskService = new TaskService();
+        taskService.add(dailyTask);
+        taskService.add(oneTimeTask);
+        taskService.add(monthlyTask);
+
+        try {
+            System.out.println(taskService.remove(3));
+        } catch (TaskNotFoundExceptions e) {
+            System.out.println("Нельзя удалить! Задачи не существует! ");
+        }
+
+        System.out.println(taskService.getAllByDate(LocalDate.of(2023, 12, 13)));
     }
 }
---
+++ src/services/TaskService.java
@@ -0,0 +1,40 @@
+package services;
+
+import exceptions.TaskNotFoundExceptions;
+import task.Task;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class TaskService {
+    private final Map<Integer, Task> taskMap;
+    private final Collection<Task> removedTasks;
+
+    public TaskService() {
+        this.taskMap = new HashMap<>();
+        this.removedTasks = new ArrayList<>();
+    }
+
+    public void add(Task task) {
+        taskMap.put(task.getId(), task);
+    }
+
+    public Task remove (int id){
+        if (taskMap.containsKey(id)) {
+            Task tempTask = taskMap.get(id);
+            removedTasks.add(tempTask);
+            taskMap.remove(id);
+            return tempTask;
+        } else {
+            throw new TaskNotFoundExceptions();
+        }
+    }
+
+    public Collection<Task> getAllByDate(LocalDate localDate) {
+        return taskMap.values().stream().filter(task -> task.appearsIn(localDate)).toList();
+    }
+}
